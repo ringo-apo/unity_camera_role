46e03303fdf8f42060efa558d50f7b606fe48f6e
taira_akira <taira_akira@nago_etech.local>
2020-04-17 13:44:31 +0900

    20200417

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float target_rotate = 100;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var target = Quaternion.Euler(new Vector3(target_rotate, 0, 0));
+
+        var now_rot = transform.rotation;
+
+        if (Quaternion.Angle(now_rot, target) <= 1)
+        {
+            transform.rotation = target;
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0.0f, 0.2f, 0.5f));
+        }
+
+    }
+}
